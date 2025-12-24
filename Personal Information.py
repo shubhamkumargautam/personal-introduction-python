8dd8f3147d50b676bc512b062f3c8ad661cf642b
shubhamkumargautam <128667108+shubhamkumargautam@users.noreply.github.com>
2025-12-24 18:48:49 +0530

    Add files via upload

---
+++ Personal Information.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+age = input("How old are you?")
+hobby = input("What is your favorite hobby?")
+City = input("Which city do you live in?")
+
+print("Welcome",name)
+print("You are",age,"year old")
+print("You love",hobby)
+print("You live in",City)
